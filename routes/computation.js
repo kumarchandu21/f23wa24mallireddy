6000a01e8115ad2ad84061cd43451e36de7a8ec9
Kumar Chandu <kumarchandu218@gmail.com>
2023-10-25 23:55:44 -0500

    data updated

--- routes/computation.js
+++ routes/computation.js
@@ -1,12 +1,7 @@
 var express = require('express');
-//const { render } = require('../app');
+
 var router = express.Router();
 
-/*let r1 = Math.random() * 10;
-let r2 = Math.random() * 20;
-let n1 = 0;
-let n2 = 0;
-let n3 = 0;*/
 
  router.get('/', function(req, res, next){
   if(Object.keys(req.query).length == 0){
@@ -24,19 +19,5 @@ val3:`Math.round() is ${x} is ${Math.round(x)}`});
 }
  
 });
-   /*n1 = Math.round(r1);
-   n2 = Math.round(r2);
-   n3 = Math.round(r1);
-
-   res.send(`Random Numbers are ${r1} and ${r2}
   
-   
-   \n <br>
-   Rounded value of ${r1} is ${n1}
-   
-   \n <br>
-   Rounded value of ${r2} is ${n2}
-   
-   `);
- });*/
  module.exports = router;
