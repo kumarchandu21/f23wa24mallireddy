f4e96aef7a10c7818a4f083c7fbe5e53d3fe9bc9
Kumar Chandu <kumarchandu218@gmail.com>
2023-10-25 23:47:27 -0500

    data added

--- routes/computation.js
+++ routes/computation.js
@@ -1,5 +1,5 @@
 var express = require('express');
-const { render } = require('../app');
+//const { render } = require('../app');
 var router = express.Router();
 
 /*let r1 = Math.random() * 10;
@@ -9,17 +9,17 @@ let n2 = 0;
 let n3 = 0;*/
 
  router.get('/', function(req, res, next){
-  if(objectc.keys(req.query).length == 0){
+  if(Object.keys(req.query).length == 0){
     var x = Math.random();
   
-  res.render('computation',{val1:`Math.fround()  is ${x} to ${Math.fround(x)}`,
-val2:`Math.random() is ${x} is ${Math.round(x)}`,
-val3:`Math.round() is ${x} is ${Math.round(x)}`}); 
+  res.render('computation',{valof1:`Math.fround()  is ${x} to ${Math.fround(x)}`,
+valof2:`Math.random() is ${x} is ${Math.round(x)}`,
+valof3:`Math.round() is ${x} is ${Math.round(x)}`}); 
  }
  else {
   for(let i in req.query){
-    res.render('computation',{val1:`Math.fround()  to ${req.query[i]} is ${Math.fround(req.query[i])}`,
-    val2:`Math.fround()  to ${req.query[i]} is ${Math.random(req.query[i])}`,val3:`Math.round() to ${req.query[i]} is ${Math.round(x)}`})
+    res.render('computation',{valof1:`Math.fround()  to ${req.query[i]} is ${Math.fround(req.query[i])}`,
+    valof2:`Math.fround()  to ${req.query[i]} is ${Math.random(req.query[i])}`,valof3:`Math.round() to ${req.query[i]} is ${Math.round(x)}`})
   }
 }
  
